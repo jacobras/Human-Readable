817f9de052849b0c662cceac9e6d5fe4d0127a85
Jacob Ras <jacobras.nl@gmail.com>
2023-09-27 01:44:49 +0200

    Support singular units

--- src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadableDuration.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadableDuration.kt
@@ -8,29 +8,51 @@ import kotlin.time.Duration
  */
 internal fun formatDuration(duration: Duration): String {
     val secondsAgo = duration.inWholeSeconds
+    val hoursAgo = duration.inWholeHours
     val daysAgo = duration.inWholeDays
-    val weeksAgo = duration.inWholeDays / 7f
-    val monthsAgo = duration.inWholeDays / 30.5f
+    val weeksAgo = (duration.inWholeDays / 7f).roundToInt()
+    val monthsAgo = (duration.inWholeDays / 30.5f).roundToInt()
     val yearsAgo = duration.inWholeDays / 365
 
     return when {
+        secondsAgo == 1L -> {
+            "1 second"
+        }
         secondsAgo < 60 -> {
             "$secondsAgo seconds"
+        }
+        secondsAgo == 60L -> {
+            "1 minute"
         }
         secondsAgo < 3600 -> {
             "${duration.inWholeMinutes} minutes"
         }
+        hoursAgo == 1L -> {
+            "1 hour"
+        }
         daysAgo < 1 -> {
             "${duration.inWholeHours} hours"
+        }
+        daysAgo == 1L -> {
+            "1 day"
         }
         daysAgo < 7 -> {
             "$daysAgo days"
         }
+        weeksAgo == 1 -> {
+            "1 week"
+        }
         daysAgo < 30 -> {
-            "${weeksAgo.roundToInt()} weeks"
+            "$weeksAgo weeks"
+        }
+        monthsAgo == 1 -> {
+            "1 month"
         }
         monthsAgo < 12 -> {
-            "${monthsAgo.roundToInt()} months"
+            "$monthsAgo months"
+        }
+        yearsAgo == 1L -> {
+            "1 year"
         }
         else -> {
             "$yearsAgo years"
--- src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableDurationTests.kt
+++ src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableDurationTests.kt
@@ -11,36 +11,44 @@ class HumanReadableDurationTests {
 
     @Test
     fun seconds() {
+        assertEquals("1 second", formatDuration(1.seconds))
         assertEquals("3 seconds", formatDuration(3.seconds))
     }
 
     @Test
     fun minutes() {
+        assertEquals("1 minute", formatDuration(1.minutes))
         assertEquals("3 minutes", formatDuration(3.minutes))
     }
 
     @Test
     fun hours() {
+        assertEquals("1 hour", formatDuration(1.hours))
         assertEquals("3 hours", formatDuration(3.hours))
+        assertEquals("23 hours", formatDuration(23.hours))
     }
 
     @Test
     fun days() {
+        assertEquals("1 day", formatDuration(1.days))
         assertEquals("3 days", formatDuration(3.days))
     }
 
     @Test
     fun weeks() {
+        assertEquals("1 week", formatDuration(7.days))
         assertEquals("3 weeks", formatDuration(21.days))
     }
 
     @Test
     fun months() {
+        assertEquals("1 month", formatDuration(30.days))
         assertEquals("3 months", formatDuration(90.days))
     }
 
     @Test
     fun years() {
+        assertEquals("1 year", formatDuration(365.days))
         assertEquals("3 years", formatDuration(1095.days))
     }
 }
