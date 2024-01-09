23e7b4856aa34e9e04d48ac09e0c23114db5a3a6
Jacob Ras <jacobras.nl@gmail.com>
2024-01-09 16:21:49 +0100

    Update Kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    kotlin("multiplatform") version "1.9.10"
+    kotlin("multiplatform") version "1.9.22"
     id("com.vanniktech.maven.publish") version "0.25.3"
 }
 
