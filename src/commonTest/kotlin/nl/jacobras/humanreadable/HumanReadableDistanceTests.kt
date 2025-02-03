9884f824f62d4ea1a7e190c92b3d1b48c5e6f0a8
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2025-02-03 10:09:45 +0100

    feat(wasm): Provide working implementation for wasm

--- src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableDistanceTests.kt
+++ src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableDistanceTests.kt
@@ -10,6 +10,7 @@ class HumanReadableDistanceTests {
         val distance = 956
         val expectedFormatted = "956 m"
         val actualFormatted = HumanReadable.distance(unit = distance, type = DistanceType.METERS)
+        println("Expected: $expectedFormatted, actual: $actualFormatted")
         assertTrue { actualFormatted == expectedFormatted }
     }
 
@@ -18,6 +19,7 @@ class HumanReadableDistanceTests {
         val distance = 1534
         val expectedFormatted = "1.5 km"
         val actualFormatted = HumanReadable.distance(unit = distance, type = DistanceType.METERS)
+        println("Expected: $expectedFormatted, actual: $actualFormatted")
         assertTrue {
             actualFormatted == expectedFormatted
         }
@@ -28,6 +30,7 @@ class HumanReadableDistanceTests {
         val distance = 5200
         val expectedFormatted = "5200 ft"
         val actualFormatted = HumanReadable.distance(unit = distance, type = DistanceType.FEET)
+        println("Expected: $expectedFormatted, actual: $actualFormatted")
         assertTrue { actualFormatted == expectedFormatted }
     }
 
@@ -36,6 +39,7 @@ class HumanReadableDistanceTests {
         val distance = 5350
         val expectedFormatted = "1.0 mi"
         val actualFormatted = HumanReadable.distance(unit = distance, type = DistanceType.FEET)
+        println("Expected: $expectedFormatted, actual: $actualFormatted")
         assertTrue { actualFormatted == expectedFormatted }
     }
 }
