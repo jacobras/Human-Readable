f9dd5955faf78aa9320085286b9706d9856b5fb3
Jacob Ras <jacobras.nl@gmail.com>
2025-02-15 00:22:50 +0100

    Replace binary `when` statements with `if`s

--- src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadableDistance.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadableDistance.kt
@@ -17,33 +17,39 @@ internal fun formatDistance(
 }
 
 private fun toMetric(meters: Float): String {
-    return when {
-        meters < 1000 -> formatNumber(meters, 0) + " m" // Use meters
-        else -> formatNumber(meters / 1000, 1) + " km" // Convert to kilometers
+    return if (meters < 1000) {
+        // Use meters
+        formatNumber(meters, 0) + " m"
+    } else {
+        // Convert to kilometers
+        formatNumber(meters / 1000, 1) + " km"
     }
 }
 
 private fun toImperial(feet: Float): String {
-    return when {
-        feet < 5280 -> formatNumber(value = feet, 0) + " ft"  // Less than a mile, use feet
-        else -> formatNumber(value = feet / 5280, 1) + " mi" // Convert to miles
+    return if (feet < 5280) {
+        // Less than a mile, use feet
+        formatNumber(value = feet, 0) + " ft"
+    } else {
+        // Convert to miles
+        formatNumber(value = feet / 5280, 1) + " mi"
     }
 }
 
 private fun formatNumber(value: Float, decimalPlaces: Int): String {
-    val result = when (decimalPlaces) {
-        0 -> value.roundToInt()
-        else -> {
-            val factor = 10.0.pow(decimalPlaces)
-            (value * factor) / factor
-        }
+    val result = if (decimalPlaces == 0) {
+        value.roundToInt()
+    } else {
+        val factor = 10.0.pow(decimalPlaces)
+        (value * factor) / factor
     }
     val separatorIndex = result.toString().indexOf(".")
     val isSeparatorFound = separatorIndex > 0
     if (isSeparatorFound) {
-        val substringAccordingToDecimalPlaces = when (decimalPlaces) {
-            0 -> result.toString().substring(0, separatorIndex)
-            else -> result.toString().substring(0, separatorIndex + 1 + decimalPlaces)
+        val substringAccordingToDecimalPlaces = if (decimalPlaces == 0) {
+            result.toString().substring(0, separatorIndex)
+        } else {
+            result.toString().substring(0, separatorIndex + 1 + decimalPlaces)
         }
         return substringAccordingToDecimalPlaces
     } else {
