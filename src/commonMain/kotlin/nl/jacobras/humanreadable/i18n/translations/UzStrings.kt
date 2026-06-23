2441c32f8fdd054a6a1df64864cc79270bc1fb4c
Jacob Ras <jacobras.nl@gmail.com>
2026-06-23 22:31:55 +0200

    Fix a bunch of translation issues

--- src/commonMain/kotlin/nl/jacobras/humanreadable/i18n/translations/UzStrings.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/i18n/translations/UzStrings.kt
@@ -1,21 +1,42 @@
 package nl.jacobras.humanreadable.i18n.translations
 
 import nl.jacobras.humanreadable.i18n.Plural
-import nl.jacobras.humanreadable.i18n.presentTense
 import nl.jacobras.humanreadable.i18n.DateTimeStrings
 import nl.jacobras.humanreadable.i18n.HumanReadableStrings
 import nl.jacobras.humanreadable.i18n.NumberStrings
+import nl.jacobras.humanreadable.i18n.multipleTenses
 
 internal val UzStrings = HumanReadableStrings(
     dateTime = DateTimeStrings(
         plural = { count -> if (count == 1) Plural.One else Plural.Other },
-        seconds = presentTense(other = "soniya"),
-        minutes = presentTense(other = "daqiqa"),
-        hours = presentTense(other = "soat"),
-        days = presentTense(other = "kun"),
-        weeks = presentTense(other = "hafta"),
-        months = presentTense(other = "oy"),
-        years = presentTense(other = "yil"),
+        seconds = multipleTenses {
+            present(one = "soniya", other = "soniya")
+            future(one = "soniyadan", other = "soniyadan")
+        },
+        minutes = multipleTenses {
+            present(one = "daqiqa", other = "daqiqa")
+            future(one = "daqiqadan", other = "daqiqadan")
+        },
+        hours = multipleTenses {
+            present(one = "soat", other = "soat")
+            future(one = "soatdan", other = "soatdan")
+        },
+        days = multipleTenses {
+            present(one = "kun", other = "kun")
+            future(one = "kundan", other = "kundan")
+        },
+        weeks = multipleTenses {
+            present(one = "hafta", other = "hafta")
+            future(one = "haftadan", other = "haftadan")
+        },
+        months = multipleTenses {
+            present(one = "oy", other = "oy")
+            future(one = "oydan", other = "oydan")
+        },
+        years = multipleTenses {
+            present(one = "yil", other = "yil")
+            future(one = "yildan", other = "yildan")
+        },
         timeAgo = { "$it oldin" },
         timeInFuture = { "$it keyin" },
         now = "hozir"
