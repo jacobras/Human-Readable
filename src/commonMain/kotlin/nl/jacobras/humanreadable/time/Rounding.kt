d998e6ade680a1671c75baa5be36c86b9753084d
Jacob Ras <jacobras.nl@gmail.com>
2026-06-24 22:24:37 +0200

    Add `UpIfClose` rounding

--- src/commonMain/kotlin/nl/jacobras/humanreadable/time/Rounding.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/time/Rounding.kt
@@ -21,4 +21,16 @@ public sealed interface Rounding {
      * - `14.days` results in "2 weeks".
      */
     public data object HalfUp : Rounding
+
+    /**
+     * Rounds up if the duration is close to the next unit.
+     *
+     * "Close" is defined as:
+     * - `55.seconds` rounds up to a minute
+     * - `55.minutes` rounds up to an hour
+     * - `23.hours` rounds up to a day
+     *
+     * Larger units are rounded half up, e.g. 10.5 days rounds up to 11 days.
+     */
+    public data object UpIfClose : Rounding
 }
