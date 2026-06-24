8787ac126d720e8df48f0ebb2ed45fbf5f393378
Jacob Ras <jacobras.nl@gmail.com>
2026-06-24 20:52:10 +0200

    Improve test stability

--- src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableRelativeTimeTests.kt
+++ src/commonTest/kotlin/nl/jacobras/humanreadable/HumanReadableRelativeTimeTests.kt
@@ -2,6 +2,7 @@ package nl.jacobras.humanreadable
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import nl.jacobras.humanreadable.localized.LocalisedTests
 import kotlin.test.Test
 import kotlin.time.Clock
 import kotlin.time.Duration.Companion.days
@@ -9,6 +10,11 @@ import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
+/**
+ * Main test for the specific configuration options related to relative time formatting.
+ *
+ * All in English, see [LocalisedTests] for specific tests for the supported languages.
+ */
 @OptIn(ExperimentalTime::class)
 class HumanReadableRelativeTimeTests {
 
@@ -21,16 +27,16 @@ class HumanReadableRelativeTimeTests {
 
     @Test
     fun inFuture() {
-        assertThat(HumanReadable.timeAgo(now + 200.seconds)).isEqualTo("in 3 minutes")
+        assertThat(HumanReadable.timeAgo(now + 200.seconds, baseInstant = now)).isEqualTo("in 3 minutes")
     }
 
     @Test
     fun now() {
-        assertThat(HumanReadable.timeAgo(now)).isEqualTo("now")
+        assertThat(HumanReadable.timeAgo(now, baseInstant = now)).isEqualTo("now")
     }
 
     @Test
     fun inPast() {
-        assertThat(HumanReadable.timeAgo(now - 3.days)).isEqualTo("3 days ago")
+        assertThat(HumanReadable.timeAgo(now - 3.days, baseInstant = now)).isEqualTo("3 days ago")
     }
 }
