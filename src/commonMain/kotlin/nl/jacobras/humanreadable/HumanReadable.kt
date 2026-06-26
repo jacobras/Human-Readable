5217a0dbe5debef2b78211fce93b1f847cbfcdbe
jacobras <jacobras.nl@gmail.com>
2026-06-26 15:03:47 +0200

    Remove wildcard import

--- src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadable.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadable.kt
@@ -14,7 +14,13 @@ import nl.jacobras.humanreadable.HumanReadable.languageTag
 import nl.jacobras.humanreadable.HumanReadable.number
 import nl.jacobras.humanreadable.i18n.HumanReadableStrings
 import nl.jacobras.humanreadable.i18n.Localisation
-import nl.jacobras.humanreadable.time.*
+import nl.jacobras.humanreadable.time.FormatStyle
+import nl.jacobras.humanreadable.time.Parts
+import nl.jacobras.humanreadable.time.RelativeTime
+import nl.jacobras.humanreadable.time.Rounding
+import nl.jacobras.humanreadable.time.TimeUnit
+import nl.jacobras.humanreadable.time.formatDuration
+import nl.jacobras.humanreadable.time.formatTimeAgo
 import kotlin.time.Clock
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
