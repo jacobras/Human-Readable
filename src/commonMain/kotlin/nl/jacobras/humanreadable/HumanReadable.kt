1c188b09eb3f09093fc1cbf2aa4eff1c121e205c
Jacob Ras <jacobras.nl@gmail.com>
2026-06-30 09:12:11 +0200

    Make yesterday/today/tomorrow work for `LocalDate`s

--- src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadable.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/HumanReadable.kt
@@ -2,11 +2,8 @@
 
 package nl.jacobras.humanreadable
 
-import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.minus
-import kotlinx.datetime.plus
-import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.todayIn
 import nl.jacobras.humanreadable.HumanReadable.duration
 import nl.jacobras.humanreadable.HumanReadable.fallbackLanguageTag
@@ -64,7 +61,6 @@ public object HumanReadable {
      * @param instant The [Instant] to format.
      * @param baseInstant The base/starting [Instant], defaulting to "now".
      * @param formatStyle The [FormatStyle] to use, defaulting to [FormatStyle.Long].
-     * @param timeZone If set, today/tomorrow/yesterday will be used.
      * @param parts Configures the formatting of multiple parts, defaulting to 1 part.
      * @param units The [TimeUnit]s to limit to during formatting, not limited by default.
      * @param rounding The [Rounding] strategy to use, defaulting to [Rounding.HalfUp].
@@ -75,31 +71,48 @@ public object HumanReadable {
         instant: Instant,
         baseInstant: Instant = Clock.System.now(),
         formatStyle: FormatStyle = FormatStyle.Long,
-        timeZone: TimeZone? = null,
         parts: Parts = Parts(),
         units: Set<TimeUnit> = TimeUnit.all,
         rounding: Rounding = Rounding.HalfUp
     ): String {
-        if (timeZone != null) {
-            val localDate = instant.toLocalDateTime(timeZone).date
-            val today = Clock.System.todayIn(timeZone)
-
-            when (localDate) {
-                today.minus(1, DateTimeUnit.DAY) -> return strings.dateTime.yesterday
-                today -> return strings.dateTime.today
-                today.plus(1, DateTimeUnit.DAY) -> return strings.dateTime.tomorrow
-            }
-        }
-
         return formatTimeAgo(
             instant = instant,
             baseInstant = baseInstant,
             formatStyle = formatStyle,
-            timeZone = timeZone,
             parts = parts,
             units = units,
             rounding = rounding
+        )
+    }
 
+    /**
+     * Returns the difference between [baseDate] and [date], in human-readable format. Also supports
+     * dates in the future or past. For example, a date that's 2 days ago will return "2 days ago".
+     *
+     * @param date The [LocalDate] to format.
+     * @param baseDate The base/starting [LocalDate], defaulting to "today".
+     * @param formatStyle The [FormatStyle] to use, defaulting to [FormatStyle.Long].
+     * @param parts Configures the formatting of multiple parts, defaulting to 1 part.
+     * @param units The [TimeUnit]s to limit to during formatting, not limited by default.
+     * @param rounding The [Rounding] strategy to use, defaulting to [Rounding.HalfUp].
+     * @return a formatted string
+     */
+    @OptIn(ExperimentalTime::class)
+    public fun timeAgo(
+        date: LocalDate,
+        baseDate: LocalDate = Clock.System.todayIn(TimeZone.currentSystemDefault()),
+        formatStyle: FormatStyle = FormatStyle.Long,
+        parts: Parts = Parts(),
+        units: Set<TimeUnit> = TimeUnit.all,
+        rounding: Rounding = Rounding.HalfUp
+    ): String {
+        return formatTimeAgo(
+            date = date,
+            baseDate = baseDate,
+            formatStyle = formatStyle,
+            parts = parts,
+            units = units,
+            rounding = rounding
         )
     }
 
--- src/commonTest/kotlin/nl/jacobras/humanreadable/time/HumanReadableRelativeTimeTests.kt
+++ src/commonTest/kotlin/nl/jacobras/humanreadable/time/HumanReadableRelativeTimeTests.kt
@@ -2,7 +2,10 @@ package nl.jacobras.humanreadable.time
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import kotlinx.datetime.TimeZone
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
 import nl.jacobras.humanreadable.HumanReadable
 import nl.jacobras.humanreadable.localized.LocalisedTests
 import kotlin.test.Test
@@ -44,25 +47,24 @@ class HumanReadableRelativeTimeTests {
 
     @Test
     fun todayTomorrowYesterday() {
+        val today = LocalDate.parse("2026-07-01")
+
         assertThat(
             HumanReadable.timeAgo(
-                instant = now - 1.days,
-                baseInstant = now,
-                timeZone = TimeZone.currentSystemDefault()
+                date = today.minus(1, DateTimeUnit.DAY),
+                baseDate = today
             )
         ).isEqualTo("yesterday")
         assertThat(
             HumanReadable.timeAgo(
-                instant = now,
-                baseInstant = now,
-                timeZone = TimeZone.currentSystemDefault()
+                date = today,
+                baseDate = today
             )
         ).isEqualTo("today")
         assertThat(
             HumanReadable.timeAgo(
-                instant = now + 1.days,
-                baseInstant = now,
-                timeZone = TimeZone.currentSystemDefault()
+                date = today.plus(1, DateTimeUnit.DAY),
+                baseDate = today
             )
         ).isEqualTo("tomorrow")
     }
