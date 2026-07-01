750b555626f4193389ba4cc61e86b7476e9381f8
Jacob Ras <jacobras.nl@gmail.com>
2026-07-01 08:11:28 +0200

    Fix `Rounding.HalfUp`

--- src/commonMain/kotlin/nl/jacobras/humanreadable/time/Rounding.kt
+++ src/commonMain/kotlin/nl/jacobras/humanreadable/time/Rounding.kt
@@ -1,5 +1,10 @@
 package nl.jacobras.humanreadable.time
 
+/**
+ * Rounding method.
+ *
+ * Note about multi-parts (when [Parts.max] > 1): rounding is done on the smallest included unit.
+ */
 public sealed interface Rounding {
 
     /**
@@ -17,6 +22,8 @@ public sealed interface Rounding {
      * Some examples:
      * - `12.days` results in "2 weeks".
      * - `46.days` results in "2 months".
+     *
+     * Note that when [Parts.max] is larger than 1, the rounding is done on the smallest unit.
      */
     public data object HalfUp : Rounding
 
@@ -29,7 +36,7 @@ public sealed interface Rounding {
      * - `23.hours` rounds up to a day.
      * - `13.days` rounds up to two weeks.
      *
-     * Anything else is rounded **down**.
+     * Anything else is rounded **down** (see [Floor]).
      */
     public data object UpIfClose : Rounding
 }
